1c2e4e1fc86949165c541ebf17195e86cb1f1a45
Karl Beecken <karl@beecken.berlin>
2022-10-24 18:43:12 +0200

    Initial Commit

---
+++ DS100ify/DS100ifyApp.swift
@@ -0,0 +1,20 @@
+//
+//  DS100ifyApp.swift
+//  DS100ify
+//
+//  Created by Karl Beecken on 24.10.22.
+//
+
+import SwiftUI
+
+@main
+struct DS100ifyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
